6da1c3a4884035862e5424550573abc55e02ba78
Dusu Park <wow@megaworks.ai>
2023-03-06 14:47:04 +0900

    add

--- get_fitbit_data.js
+++ get_fitbit_data.js
@@ -1,9 +1,10 @@
 
-const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzk2WVkiLCJzdWIiOiJCR0hRWDQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJwcm8gcm51dCByc2xlIHJjZiByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjc3ODUyNDc5LCJpYXQiOjE2Nzc4MjM2Nzl9.7PZBeG0svzboskcIwLzmmmGMqmbUozcG0KYNI2etfgw"
+const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzk2WVkiLCJzdWIiOiJCR0hRWDQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJudXQgcnBybyByc2xlIHJjZiByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjc4MTA3NzMyLCJpYXQiOjE2NzgwNzg5MzJ9.PMMMD9Qdm3k7svotqTpg0WDkLEI7hZgtm66UpJmADQU"
 
-fetch('https://api.fitbit.com/1/user/-/ hrv/date/2023-02-27/2023-03-02.json', {
+fetch('https://api.fitbit.com/1/user/-/hrv/date/today/all.json', {
   method: "GET",
   headers: {"Authorization": "Bearer " + access_token}
 })
 .then(response => response.json()) 
 .then(json => console.log(json)); 
+console.log(fetch);
