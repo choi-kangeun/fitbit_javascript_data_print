6298704ec8eb83e1bef9a87074993de3a6b9803d
Dusu Park <wow@megaworks.ai>
2023-03-09 15:02:48 +0900

    추가 수정

--- get_fitbit_data.js
+++ get_fitbit_data.js
@@ -1,6 +1,6 @@
 const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzk2WVkiLCJzdWIiOiJCR0hRWDQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJudXQgcnBybyByc2xlIHJjZiByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjc4MzQ4NTA5LCJpYXQiOjE2NzgzMTk3MDl9.tUNFHfAAslMp3yOI2-CcIrUoHpYM2mf8pjyOibiS6SE"
 
-fetch('https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json', {
+fetch('https://api.fitbit.com/1/user/-/activities/heart/date/2023-03-02/1d.json', {
   method: "GET",
   headers: {"Authorization": "Bearer " + access_token}
 })
