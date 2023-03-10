8f4ac6d3fc411948b1d9a9f6140437ba46d6eeb8
Dusu Park <wow@megaworks.ai>
2023-03-10 17:19:57 +0900

    추가수정

--- get_fitbit_data.js
+++ get_fitbit_data.js
@@ -1,6 +1,6 @@
-const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzk2WVkiLCJzdWIiOiJCR0hRWDQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJudXQgcnBybyByc2xlIHJjZiByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjc4MzQ4NTA5LCJpYXQiOjE2NzgzMTk3MDl9.tUNFHfAAslMp3yOI2-CcIrUoHpYM2mf8pjyOibiS6SE"
+const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzk2WVkiLCJzdWIiOiJCR0hRWDQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJudXQgcnBybyByc2xlIHJjZiByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjc4Mzc3NzA1LCJpYXQiOjE2NzgzNDg5MDV9.lYea2jJbSC0uE4OXH_rhKAiWiEbwE5El1OGt8v19_7Q"
 
-fetch('https://api.fitbit.com/1/user/-/activities/heart/date/2023-03-02/1d.json', {
+fetch('https://api.fitbit.com/1/user/-/activities/heart/date/2023-02-27/2023-03-06.json', {
   method: "GET",
   headers: {"Authorization": "Bearer " + access_token}
 })
